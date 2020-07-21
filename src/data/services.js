f86766c492032503faa53b77c6cfec1d9b442bfc
Minega Shyaka Patrick <minega25@gmail.com>
2020-07-21 05:42:46 +0200

    fix broken link service component

--- src/data/services.js
+++ src/data/services.js
@@ -3,7 +3,7 @@ export default [
     id: 1,
     name: "Website design",
     iconPath: "https://img.icons8.com/clouds/100/000000/web-equlizer.png",
-    link: "/services/web-design",
+    link: "/services",
     description: "We create responsive, user-friendly, and optimized websites",
     longDescription:
       "refers to the design of websites that are displayed on the internet. It usually refers to the user experience aspects of website development rather than software development. ",
@@ -12,7 +12,7 @@ export default [
     id: 2,
     name: "Web Applications",
     iconPath: "https://img.icons8.com/clouds/100/000000/web-settings.png",
-    link: "/services/web-app",
+    link: "/services",
     description: "Create a web-based tool for your amazing idea",
     longDescription:
       "A web application is a computer program that utilizes web browsers and web technology to perform tasks over the Internet.",
@@ -21,7 +21,7 @@ export default [
     id: 3,
     name: "Computer skills training",
     iconPath: "https://img.icons8.com/bubbles/100/000000/training.png",
-    link: "/services/digital-marketing",
+    link: "/services",
     description:
       "We train you to become a professional web developer and learn basic soft skills",
     longDescription:
@@ -31,7 +31,7 @@ export default [
     id: 4,
     name: "Support & Hosting",
     iconPath: "https://img.icons8.com/cute-clipart/100/000000/server.png",
-    link: "/services/support-hosting",
+    link: "/services",
     description:
       "We offer top-of-the-line hosting with daily backups and more.",
     longDescription:
@@ -41,7 +41,7 @@ export default [
     id: 5,
     name: "Mobile Applications",
     iconPath: "https://img.icons8.com/clouds/100/000000/mobile-shop-addres.png",
-    link: "/services/mobile-app",
+    link: "/services",
     description:
       "We develop visually pleasing apps that are optimized to swiftly grow your global business revenue.",
     longDescription:
@@ -51,7 +51,7 @@ export default [
     id: 6,
     name: "Graphics design & printing",
     iconPath: "https://img.icons8.com/clouds/100/000000/design.png",
-    link: "/services/graphics-design",
+    link: "/services",
     description:
       "We create visual content to communicate messages and follow through to printing",
     longDescription:
@@ -61,7 +61,7 @@ export default [
     id: 7,
     name: "Wedding photography",
     iconPath: "https://img.icons8.com/bubbles/100/000000/panorama.png",
-    link: "/services/sass",
+    link: "/services",
     description:
       "Primarily focused on the photography of events and activities relating to weddings.",
     longDescription:
@@ -71,7 +71,7 @@ export default [
     id: 8,
     name: "Wedding videography",
     iconPath: "https://img.icons8.com/clouds/100/000000/camcorder-pro.png",
-    link: "/services/videography",
+    link: "/services",
     description:
       "Primarily focused on the videography of events and activities relating to weddings.",
     longDescription:
@@ -81,7 +81,7 @@ export default [
     id: 9,
     name: "Software as a Service",
     iconPath: "https://img.icons8.com/clouds/100/000000/code.png",
-    link: "/services/sass",
+    link: "/services",
     description:
       "We provide software licensing on a subscription basis for solutions we build inhouse",
     longDescription:
@@ -91,7 +91,7 @@ export default [
     id: 10,
     name: "Digital Marketing",
     iconPath: "https://img.icons8.com/clouds/100/000000/mail-advertising.png",
-    link: "/services/digital-marketing",
+    link: "/services",
     description: "Be where your customers are: on the web. We can help.",
     longDescription:
       "Basically, digital marketing refers to any online marketing efforts or assets. Email marketing, pay-per-click advertising, social media marketing and even blogging are all great examples of digital marketing—they help introduce people to your company and convince them to buy.",
@@ -100,7 +100,7 @@ export default [
     id: 11,
     name: "Computer consultancy",
     iconPath: "https://img.icons8.com/bubbles/100/000000/information.png",
-    link: "/services/digital-marketing",
+    link: "/services",
     description:
       "We focus on advising organizations on how best to use information technology (IT) in achieving their business objectives.",
     longDescription:
